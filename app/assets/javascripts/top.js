a7afe30d69e5364ebe8d83a67762d3788f409153
unknown <>
2016-08-08 14:41:56 +0900

    地図管理_各タブのマップ表示完成

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,15 +1,23 @@
 $(window).on("load", function() {
   $("#G8_map1 > li").on("click", function() {
-    $("li.selected").removeClass("selected");
+    $("#G8_map1 > li.selected").removeClass("selected");
     $(this).addClass("selected");
     $(".contents div").hide(); // 二つの要素を非表示にする
-    $("." + this.id).show(); // クリックされたボタンに対応する要素を表示する
+    $("." + this.id).show(); //クリックされたボタンに対応する要素を表示する
+
+    if ($("#G8_map1 > li.G8_kanri").on("click")) {
+      $(".contents_kanri").show();
+      $(".map_serect").show();
+      $(".G8_kouti_kanri").show();
+    }
   });
 
   $("#G8_map2 > li").on("click", function() {
     $("#G8_map2 > li.selected").removeClass("selected");
     $(this).addClass("selected");
-    $(".contents_kanri div").hide(); // 二つの要素を非表示にする
+    $(".contents_kanri .map_serect div").hide(); // 二つの要素を非表示にする
+    $(".contents_kanri").show();
+    $(".map_serect").show();
     $("." + this.id).show(); // クリックされたボタンに対応する要素を表示する
   });
 });
